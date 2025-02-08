994969eeb13a80ce4d5834dbe53f9515b4c218f7
pstrohal <pstrohal@student.42heilbronn.de>
2025-02-08 13:26:16 +0100

    started ex02

---
+++ ex02/src/PmergeDeque.cpp
@@ -0,0 +1,8 @@
+#include "../inc/PmergeMe.hpp"
+
+void pmergeme(std::deque<int> &deq, int lvl)
+{
+    static int lvl = 1;
+
+
+}
---
+++ ex02/src/main.cpp
@@ -0,0 +1,59 @@
+#include "../inc/PmergeMe.hpp"
+
+bool compare(int &b, int &a, int &counter)
+{
+    counter++;
+    int tmp;
+    if (a < b)
+    {
+        tmp = a;
+        a = b;
+        b = tmp;
+    }
+    return;
+}
+
+int main(int argc, char **argv)
+{
+   try {
+        std::vector<int> vec;
+        std::deque<int> deq;
+        std::regex pattern("^\\d+$");
+        std::smatch match;
+
+        if (argc < 2)
+            {std::cout <<"need args!" <<std::endl;return 1;}
+        
+        for (int i = argc; i > 0; i--)
+        {
+            std::string tmp(argv[0]);
+            if (!std::regex_match(tmp, match, pattern))
+                throw std::string("Oh thats not a valid input!");
+            vec.insert(vec.end(), std::stoi(tmp));
+        }
+        
+        
+        std::cout<<"Before: ";
+        std::cout<<std::endl;
+        for_each (vec.begin(), vec.end(), [](auto i){std::cout<<*i<<" ";});
+        const auto vec_start = std::chrono::high_resolution_clock::now(); 
+        pmerge(vec, 1);
+        const auto vec_end = std::chrono::high_resolution_clock::now(); 
+        const auto deq_start = std::chrono::high_resolution_clock::now(); 
+        pmerge(deq);
+        const auto deq_end = std::chrono::high_resolution_clock::now(); 
+        std::cout<<"After: ";
+        for_each (vec.begin(), vec.end(), [](auto i){std::cout<<*i<<" ";});
+        std::cout<<std::endl;
+        auto duration_vec = std::chrono::duration_cast<std::chrono::microseconds>(vec_end - vec_start);
+        auto duration_deq = std::chrono::duration_cast<std::chrono::microseconds>(deq_end - deq_start);
+        std::cout   <<"Time to process a range of " <<argc -1 <<" elements with std::vector: " 
+                    << duration_vec.count();
+
+    }
+    catch (std::exception &e){std::cout<<"Error: "<<e.what()<<std::endl;return 1;}
+    catch (int &e){std::cout<<"ERRORCODE: "<<e<<std::endl;return 1;}
+    catch (std::string &e) {std::cout<<"ERROR: "<<e<<std::endl;return 1;}
+    catch (...) {std::cout <<"LOL!"<<std::endl;return 1;}
+
+}   
